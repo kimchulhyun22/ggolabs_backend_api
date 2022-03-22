fe6a13661c0562c8cc82136b950200519f635e79
kimchulhyun22 <rlacjfgus2@gmail.com>
2022-03-22 20:51:45 +0900

    [ADD] 카카오맵 키워드 API 테스트

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,7 @@
+import uvicorn
 from fastapi import FastAPI
+
+from app.util.kakao_map_api import search_keyword_within_radius
 
 app = FastAPI()
 
@@ -8,3 +11,10 @@ async def root():
     return {
         'Hello': 'World!'
     }
+
+
+@app.post('/api/v1/test/search/keyword')
+async def search_keyword(keyword: str, page_num: int,
+                         latitude: float, longitude: float, radius: int):
+    return await search_keyword_within_radius(keyword=keyword, page_num=page_num, latitude=latitude,
+                                              longitude=longitude, radius=radius)
