841963b5752fc2ebd2c267b81ae22914a540a9e7
kimchulhyun22 <rlacjfgus2@gmail.com>
2022-05-17 23:25:47 +0900

    [REFACTOR] Category 테스트코드 리팩토링
    - setUp / tearDown 분리

--- src/app/db/db_setup.py
+++ src/app/db/db_setup.py
@@ -1,6 +1,9 @@
 from .session import Base, engine
 
 
-def init_db():
-    Base.metadata.drop_all(bind=engine)
+def create_db():
     Base.metadata.create_all(bind=engine)
+
+
+def drop_db():
+    Base.metadata.drop_all(bind=engine)
--- src/test/models/test_category.py
+++ src/test/models/test_category.py
@@ -3,12 +3,12 @@ import unittest
 from app.db.session import SessionLocal
 from app.models.category import Category
 
+from app.db.db_setup import create_db, drop_db
+
 
 class TestCategory(unittest.TestCase):
     def setUp(self):
-        from app.db.init import init_db
-
-        init_db()
+        create_db()
         self.session = SessionLocal()
 
     def test_category(self):
@@ -21,7 +21,10 @@ class TestCategory(unittest.TestCase):
 
         # 카테고리 데이터 조회
         category = self.session.query(Category).all()[-1]
+        self.session.close()
 
         self.assertEqual(category.name, "restaurant")
         self.assertEqual(category.code, "17849")
 
+    def tearDown(self) -> None:
+        drop_db()
