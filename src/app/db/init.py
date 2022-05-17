992992113918e0bd590829861e62e6cf955ffd58
kimchulhyun22 <rlacjfgus2@gmail.com>
2022-05-17 17:29:27 +0900

    [UPDATE] Category 변경
    - click_count, image 필드 추가

--- src/app/db/init.py
+++ src/app/db/init.py
@@ -2,4 +2,5 @@ from .session import Base, engine
 
 
 def init_db():
+    Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
--- src/app/models/category.py
+++ src/app/models/category.py
@@ -8,8 +8,11 @@ class Category(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False)
     code = Column(String(50), nullable=False)
+    image = Column(String(200), nullable=True)
+    click_count = Column(Integer, default=0)
 
-    def __init__(self, name, code):
+    def __init__(self, name, code, image=None, click_count=None):
         self.name = name
         self.code = code
-        
+        self.click_count = click_count
+        self.image = image
