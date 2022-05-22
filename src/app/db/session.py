bbcdd85961c46deb815fda6a87f26047ae0b8c82
kimchulhyun22 <rlacjfgus2@gmail.com>
2022-05-22 23:33:14 +0900

    [REFACTOR] session 파일 정리

--- src/app/db/session.py
+++ src/app/db/session.py
@@ -10,13 +10,3 @@ engine = create_engine(settings.SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
-def get_db_session() -> Generator:
-    db = SessionLocal()
-
-    try:
-        yield db
-
-    finally:
-        db.close()
