9978190a991b74271e30bc60a3dc75442adf4a2e
kimchulhyun22 <rlacjfgus2@gmail.com>
2022-05-23 14:59:53 +0900

    [ADD] Category 테스트 코드 추가

---
+++ src/test/api/api_v1/test_category.py
@@ -0,0 +1,35 @@
+import unittest
+
+from app.core.config import settings
+from app.main import app
+from fastapi.testclient import TestClient
+
+
+class TestCategory(unittest.TestCase):
+    def setUp(self) -> None:
+        self.test_client = TestClient(app)
+
+    def test_category_list(self):
+        response = self.test_client.get(f"{settings.API_V1_PREFIX}/category")
+        response_json = response.json()
+
+        self.assertEqual(response.status_code, 200)
+
+        # 결과가 빈 값인 경우
+        self.assertNotEqual(response_json, None)
+        self.assertNotEqual(response_json, [])
+
+        # 필드 체크
+        response_first, response_second = response_json[0], response_json[1]
+
+        self.assertEqual('id' in response_first, True)
+        self.assertEqual('name' in response_first, True)
+        self.assertEqual('click_count' in response_first, True)
+        self.assertEqual('image' in response_first, True)
+
+        self.assertEqual('id' in response_second, True)
+        self.assertEqual('name' in response_second, True)
+        self.assertEqual('click_count' in response_second, True)
+        self.assertEqual('image' in response_second, True)
+
+        self.assertEqual(response_first['click_count'] >= response_second['click_count'], True)
