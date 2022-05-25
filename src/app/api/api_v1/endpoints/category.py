903aa8d36c594f0100303021d0a1146db248d417
kimchulhyun22 <rlacjfgus2@gmail.com>
2022-05-25 13:24:21 +0900

    [ADD] 카테고리별 상점 조회 API 추가
    get_store_list 함수 추가
    테스트 코드

--- src/app/api/api_v1/endpoints/category.py
+++ src/app/api/api_v1/endpoints/category.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import Session
 
 from app.api.deps import get_db_session
 from app.crud.crud_category import get_category_list_sorted_by_view_count
+from app.crud.crud_store import get_store_list_by_category_id
 from app.schemas.category import CategoryResponse
+from app.schemas.store import StoreResponse
 
 router = APIRouter()
 
@@ -18,5 +20,17 @@ def get_category_list(db: Session = Depends(get_db_session)) -> Any:
         status_code=200,
         data=[
             category.__dict__ for category in category_list
+        ]
+    )
+
+
+@router.get("/{category_id}/stores")
+def get_store_list(category_id: int, db: Session = Depends(get_db_session)) -> Any:
+    store_list = get_store_list_by_category_id(db=db, category_id=category_id)
+
+    return StoreResponse(
+        status_code=200,
+        data=[
+            store.__dict__ for store in store_list
         ]
     )
--- src/test/api/api_v1/test_category.py
+++ src/test/api/api_v1/test_category.py
@@ -1,20 +1,30 @@
 import unittest
 
 from app.core.config import settings
+from app.db.init_db import drop_db
+from app.db.session import SessionLocal
 from app.main import app
 from fastapi.testclient import TestClient
+
+from app.models.store import Store
 
 
 class TestCategory(unittest.TestCase):
     def setUp(self) -> None:
         self.test_client = TestClient(app)
 
+        self.test_store = [
+            ["matjip1", "POINT(126.5312 33.4996)"],
+            ["matjip2", "POINT(126.5312 33.4996)"],
+            ["matjip3", "POINT(126.5312 33.4996)"],
+            ["matjip4", "POINT(126.5312 33.4996)"]
+        ]
+
     def test_category_list(self):
         response = self.test_client.get(f"{settings.API_V1_PREFIX}/categories")
         response_json = response.json()
 
         self.assertEqual(response.status_code, 200)
-
         # 결과가 빈 값인 경우
         self.assertNotEqual(response_json, None)
         self.assertNotEqual(response_json, [])
@@ -33,3 +43,36 @@ class TestCategory(unittest.TestCase):
         self.assertEqual('image' in response_second, True)
 
         self.assertEqual(response_first['view_count'] >= response_second['view_count'], True)
+
+    def test_store_list(self):
+        session = SessionLocal()
+
+        for store_name, store_location in self.test_store:
+            store = Store(name=store_name,
+                          location=store_location)
+            store.category_id = 1
+            session.add(store)
+            session.commit()
+
+        session.close()
+
+        response = self.test_client.get(f"{settings.API_V1_PREFIX}/categories/1/stores")
+        response_json = response.json()
+        response_json = response_json["data"]
+
+        self.assertEqual(response.status_code, 200)
+
+        # 결과가 빈 값인 경우
+        self.assertNotEqual(response_json, None)
+        self.assertNotEqual(response_json, [])
+
+        # 필드 체크
+        self.assertEqual("id" in response_json[0], True)
+        self.assertEqual("name" in response_json[0], True)
+        self.assertEqual("x" in response_json[0], True)
+        self.assertEqual("y" in response_json[0], True)
+        self.assertEqual("view_count" in response_json[0], True)
+        self.assertEqual("image" in response_json[0], True)
+
+    def tearDown(self) -> None:
+        drop_db()
