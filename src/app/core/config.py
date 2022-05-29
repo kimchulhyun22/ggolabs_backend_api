2057b1ad2c46a5d5d71c126ee4c5069d709c93a4
kimchulhyun22 <rlacjfgus2@gmail.com>
2022-05-29 23:20:55 +0900

    [ADD] AWS DB config 추가

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -12,4 +12,15 @@ class LocalSettings(BaseSettings):
     SQLALCHEMY_DATABASE_URL = f'postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
 
 
-settings = LocalSettings()
+class Settings(BaseSettings):
+    # database connection
+    HOST: str = "ggolabs.ckst523n2vqd.ap-northeast-2.rds.amazonaws.com"
+    PORT: int = 5432
+    USERNAME: str = "root"
+    PASSWORD: str = "sdfsdfsdf"
+    DATABASE: str = "postgres"
+    API_V1_PREFIX: str = "/api/v1"
+    SQLALCHEMY_DATABASE_URL = f'postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
+
+
+settings = Settings()
